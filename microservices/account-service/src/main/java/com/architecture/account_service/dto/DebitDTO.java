28941d975fc3e5897b6208e4bf98ed6ce6d503d1
Giovanni Marques <giovanni.marques030@gmail.com>
2025-10-01 09:51:00 -0300

    fix: add static in the dtos

--- microservices/account-service/src/main/java/com/architecture/account_service/dto/DebitDTO.java
+++ microservices/account-service/src/main/java/com/architecture/account_service/dto/DebitDTO.java
@@ -3,6 +3,6 @@ package com.architecture.account_service.dto;
 import java.math.BigDecimal;
 
 public abstract class DebitDTO {
-    public record Input(Long accountId, BigDecimal amount) {
+    public static record Input(Long accountId, BigDecimal amount) {
     };
 }
--- microservices/account-service/src/main/java/com/architecture/account_service/dto/ProfileDTO.java
+++ microservices/account-service/src/main/java/com/architecture/account_service/dto/ProfileDTO.java
@@ -3,9 +3,9 @@ package com.architecture.account_service.dto;
 import java.math.BigDecimal;
 
 public abstract class ProfileDTO {
-    public record Input(Long accountId) {
+    public static record Input(Long accountId) {
     };
 
-    public record Output(Long accountId, BigDecimal balance, String name, String email, String phone) {
+    public static record Output(Long accountId, BigDecimal balance, String name, String email, String phone) {
     };
 }

35cf17576b4f5e00d9253fe438fa88d2682082e8
Gabriel Alves <gabrielhalves03@gmail.com>
2025-10-04 12:18:13 -0300

    fix: load test

--- antifraud-service/load-tests/load-test.js
+++ antifraud-service/load-tests/load-test.js
@@ -3,20 +3,19 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-    { duration: "30s", target: 20 },
-    { duration: "1m", target: 20 },
+    { duration: "30s", target: 50 },
+    { duration: "1m", target: 50 },
     { duration: "30s", target: 0 },
   ],
 };
 
 export default function () {
-  const url = "http://fraud-service:8081/api/fraud/validate";
+  const url = "http://fraud-service:8081/v1/fraud/validate";
   const payload = JSON.stringify({
     transactionId: `${__VU}-${__ITER}`,
     amount: Math.floor(Math.random() * 20000),
     transactionType: "TRANSFER",
-    sourceAccount: "ACC123",
-    destinationAccount: "ACC456",
+    accountId: "6",
   });
 
   const params = {

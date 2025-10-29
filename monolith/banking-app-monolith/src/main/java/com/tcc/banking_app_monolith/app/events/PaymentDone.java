1f60693213b0620e1fffaad77f2d629cbcf18286
Gabriel Alves <gabrielhalves03@gmail.com>
2025-10-29 19:46:41 -0300

    feat: add PaymentDone class for handling completed payment events

---
+++ monolith/banking-app-monolith/src/main/java/com/tcc/banking_app_monolith/app/events/PaymentDone.java
@@ -0,0 +1,44 @@
+package com.tcc.banking_app_monolith.app.events;
+
+import com.tcc.banking_app_monolith.domain.enums.TransactionType;
+
+import java.math.BigDecimal;
+
+public class PaymentDone {
+
+    public static String queue = "payment.done.queue";
+
+    private Long accountId;
+    private BigDecimal amount;
+    private TransactionType type;
+
+    public PaymentDone(Long accountId, BigDecimal amount, TransactionType type) {
+        this.accountId = accountId;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+}

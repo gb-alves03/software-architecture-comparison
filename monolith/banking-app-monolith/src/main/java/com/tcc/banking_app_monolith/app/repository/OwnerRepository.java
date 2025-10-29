5112903d6611e15b76783f720f86589c2e71b2c2
Gabriel Alves <gabrielhalves03@gmail.com>
2025-10-29 19:46:46 -0300

    feat: add findByEmail method to OwnerRepository for retrieving owners by email

--- monolith/banking-app-monolith/src/main/java/com/tcc/banking_app_monolith/app/repository/OwnerRepository.java
+++ monolith/banking-app-monolith/src/main/java/com/tcc/banking_app_monolith/app/repository/OwnerRepository.java
@@ -4,6 +4,9 @@ import com.tcc.banking_app_monolith.domain.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
+    Optional<Owner> findByEmail(String email);
 }

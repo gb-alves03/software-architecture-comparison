614f4f51d7184fc4bca888e44f3f907707242e5d
Gabriel Alves <gabrielhalves03@gmail.com>
2025-11-02 02:38:48 -0300

    feat: rename Queue interface to AccountQueue for clarity

--- monolith/banking-app-monolith/src/main/java/com/tcc/banking_app_monolith/infra/queue/AccountQueue.java
+++ monolith/banking-app-monolith/src/main/java/com/tcc/banking_app_monolith/infra/queue/AccountQueue.java
@@ -2,7 +2,7 @@ package com.tcc.banking_app_monolith.infra.queue;
 
 import java.util.function.Function;
 
-public interface Queue {
+public interface AccountQueue {
 
     void publish(String exchange, String routingKey, Object data);
 
